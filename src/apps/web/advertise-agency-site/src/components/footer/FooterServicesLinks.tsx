c4ac275cd18bb6e9ec3cf7f297db7539d1acc571
Maxim Matveev <maxx.matveev@gmail.com>
2025-03-31 19:35:09 +0300

    [AdvAgencySite] [Feature] Доработан footer. Ссылки на услуги
    - Добавлены подразделы
    - Поработан компонент FooterServicesLinks

--- src/apps/web/advertise-agency-site/src/components/footer/FooterServicesLinks.tsx
+++ src/apps/web/advertise-agency-site/src/components/footer/FooterServicesLinks.tsx
@@ -1,26 +1,80 @@
 import Link from 'next/link';
 import React from "react";
+import { cn } from "@/lib/utils";
 
-interface ServiceLink {
+interface ServiceItem {
     name: string;
-    href: string;
+    href?: string;
+    subItems?: {
+        name: string;
+        href: string;
+    }[];
 }
 
 interface ServicesLinksProps {
-    servicesLinks: ServiceLink[];
+    servicesLinks: ServiceItem[];
+    className?: string;
 }
 
-export default function FooterServicesLinks({ servicesLinks }: ServicesLinksProps): React.JSX.Element {
+export default function FooterServicesLinks({
+                                                servicesLinks,
+                                                className
+                                            }: ServicesLinksProps): React.JSX.Element {
     return (
-        <ul className="space-y-2">
-            {servicesLinks.map((link) => (
-                <li key={link.name}>
-                    <Link href={link.href}
-                          className={`text-gray-600 hover:text-gray-900 dark:text-gray-400 
-                          dark:hover:text-white transition-colors hover:underline 
-                          hover:underline-offset-4 hover:decoration-current`}>
-                        {link.name}
-                    </Link>
+        <ul className={cn("space-y-4", className)}> {/* Уменьшено с space-y-8 */}
+            {servicesLinks.map((service) => (
+                <li key={service.name}>
+                    <div className="mb-2"> {/* Уменьшено с mb-4 */}
+                        {service.href ? (
+                            <Link
+                                href={service.href}
+                                className={cn(
+                                    "font-medium block",
+                                    "text-gray-900 hover:text-gray-600",
+                                    "dark:text-white dark:hover:text-gray-300",
+                                    "transition-colors",
+                                    "hover:underline hover:underline-offset-4 hover:decoration-current",
+                                    "pb-1"
+                                )}
+                            >
+                                {service.name}
+                            </Link>
+                        ) : (
+                            <span className={cn(
+                                "font-medium block",
+                                "text-gray-900 dark:text-white",
+                                "hover:underline hover:underline-offset-4 hover:decoration-current",
+                                "cursor-default",
+                                "pb-1"
+                            )}>
+                                {service.name}
+                            </span>
+                        )}
+                    </div>
+
+                    {service.subItems && (
+                        <ul className={cn(
+                            "pl-5 space-y-2",
+                            "border-l-1 border-gray-400 dark:border-gray-700",
+                            "ml-2"
+                            )}>
+                            {service.subItems.map((subItem) => (
+                                <li key={subItem.name}>
+                                    <Link
+                                        href={subItem.href}
+                                        className={cn(
+                                            "text-gray-600 hover:text-gray-900",
+                                            "dark:text-gray-400 dark:hover:text-white",
+                                            "transition-colors",
+                                            "hover:underline hover:underline-offset-4 hover:decoration-current"
+                                        )}
+                                    >
+                                        - {subItem.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
                 </li>
             ))}
         </ul>
