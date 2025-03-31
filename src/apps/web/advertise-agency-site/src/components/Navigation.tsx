cb78234a293d4cb145b96135f6d41f2f30920b04
Maxim Matveev <maxx.matveev@gmail.com>
2025-03-31 05:17:30 +0300

    [AdvAgencySite] [Feature] Доработан тулбар
    - Кнопки вынесены в компонент Toolbar
    - Для мобильной версии Toolbar отображается в навигации

--- src/apps/web/advertise-agency-site/src/components/Navigation.tsx
+++ src/apps/web/advertise-agency-site/src/components/Navigation.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Link from "next/link";
 import {Button} from "@/components/ui/button";
 import { usePathname } from "next/navigation"
+import Toolbar from "@/components/Toolbar";
 
 export default function Navigation() {
     const pathname = usePathname()
@@ -17,28 +18,29 @@ export default function Navigation() {
     ]
 
     return (
-        <>
-            <div className="border-b border-gray-200 dark:border-gray-800"> {/* Добавленная линия */}
-                <div className="container mx-auto px-4">
-                    {/* Десктопное меню - выровнено по центру */}
-                    <nav className="hidden md:flex justify-center w-full py-4"> {/* Добавлен padding */}
-                        <div className="flex items-center space-x-2">
-                            {navLinks.map((link) => (
-                                <Button
-                                    key={link.href}
-                                    asChild
-                                    variant={pathname === link.href ? "secondary" : "ghost"}
-                                    className="hover:text-primary"
-                                >
-                                    <Link href={link.href}>
-                                        {link.name}
-                                    </Link>
-                                </Button>
-                            ))}
-                        </div>
-                    </nav>
-                </div>
+        <div className="border-b border-gray-200 dark:border-gray-800"> {/* Добавленная линия */}
+            <div className="container mx-auto px-4">
+                {/* Десктопное меню - выровнено по центру */}
+                <nav className="hidden md:flex justify-center w-full py-4"> {/* Добавлен padding */}
+                    <div className="flex items-center space-x-2">
+                        {navLinks.map((link) => (
+                            <Button
+                                key={link.href}
+                                asChild
+                                variant={pathname === link.href ? "secondary" : "ghost"}
+                                className="hover:text-primary"
+                            >
+                                <Link href={link.href}>
+                                    {link.name}
+                                </Link>
+                            </Button>
+                        ))}
+                    </div>
+                </nav>
+
+                {/* Мобильная версия */}
+                <Toolbar variant={"mobile"} />
             </div>
-        </>
+        </div>
     );
 }
