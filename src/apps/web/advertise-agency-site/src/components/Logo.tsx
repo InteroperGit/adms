e8a771d69e009ccfdc29bfdcb6dc099b83466397
Maxim Matveev <maxx.matveev@gmail.com>
2025-03-31 03:24:41 +0300

    [AdvAgencySite] [Feature] Разработана базовая версия layout
    - Разработана базовая версия layout
    - Добавлены сторонние компоненты shadcn

---
+++ src/apps/web/advertise-agency-site/src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { GiButterfly } from "react-icons/gi";
+
+export default function Logo() {
+    return (
+        <Link href="/" className={"inline-flex items-center text-xl font-bold text-primary mr-auto"}>
+            <GiButterfly className="text-blue-500 text-2xl mr-2" />
+            <span className={"text-orange-500"}>Рекла</span>
+            <span className={"text-blue-500"}>Мастер</span>
+        </Link>
+    );
+}
