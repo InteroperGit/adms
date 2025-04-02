88dace168c2356ce0f2256f6aa28c977dee5b4fc
Maxim Matveev <maxx.matveev@gmail.com>
2025-04-03 01:53:01 +0300

    [AdvAgencySite] [Feature] Добавлен компонент ClientsSection
        - Добавлен компонент ClientsSection
        - Доработана главная страница
        - Поправлена визуализация Footer

--- src/apps/web/advertise-agency-site/src/components/footer/Footer.tsx
+++ src/apps/web/advertise-agency-site/src/components/footer/Footer.tsx
@@ -39,58 +39,56 @@ export default function Footer() {
     ]
 
     return (
-        <footer className="bg-gray-50 dark:bg-gray-900 border-t">
-            <div className="container px-8 py-12 mx-auto">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grud-cols-4 gap-8">
-                    {/* Лого */}
-                    <div className="space-y-4">
-                        <Logo />
-                        <p className="text-gray-600 dark:text-gray-400">
-                            Профессиональные решения для вашего бизнеса
-                        </p>
-                    </div>
-
-                    {/* Пустая ячейка */}
-                    <div className="space-y-4"></div>
-
-                    {/* Социальные сети с икноками */}
-                    <div className="space-y-4">
-                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                            Мы в социальных сетях:
-                        </h3>
-                        <FooterSocialIcons socialLinks={socialLinks} className="mt-4" />
-                    </div>
-
-                    {/* Ссылки Компания */}
-                    <div className="space-y-4">
-                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                            Компания
-                        </h3>
-                        <FooterCompanyLinks companyLinks={companyLinks} />
-                    </div>
-
-                    {/* Ссылки Услуги */}
-                    <div className="space-y-4">
-                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                            Услуги
-                        </h3>
-                        <FooterServicesLinks servicesLinks={servicesLinks} />
-                    </div>
-
-                    {/* Контакты */}
-                    <div className="space-y-3">
-                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                            Контакты
-                        </h3>
-                        <FooterContacts className="mt-4" />
-                    </div>
+        <footer className="bg-gray-50 dark:bg-gray-900 border-t p-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grud-cols-4 gap-8">
+                {/* Лого */}
+                <div className="space-y-4">
+                    <Logo />
+                    <p className="text-gray-600 dark:text-gray-400">
+                        Профессиональные решения для вашего бизнеса
+                    </p>
                 </div>
 
-                {/* Копирайт */}
-                <div
-                    className="border-t border-gray-200 dark:border-gray-800 mt-12 pt-8 text-center text-gray-500 dark:text-gray-400 text-sm">
-                    © {currentYear} Рекламастер. Все права защищены.
+                {/* Пустая ячейка */}
+                <div className="space-y-4"></div>
+
+                {/* Социальные сети с икноками */}
+                <div className="space-y-4">
+                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                        Мы в социальных сетях:
+                    </h3>
+                    <FooterSocialIcons socialLinks={socialLinks} className="mt-4" />
                 </div>
+
+                {/* Ссылки Компания */}
+                <div className="space-y-4">
+                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                        Компания
+                    </h3>
+                    <FooterCompanyLinks companyLinks={companyLinks} />
+                </div>
+
+                {/* Ссылки Услуги */}
+                <div className="space-y-4">
+                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                        Услуги
+                    </h3>
+                    <FooterServicesLinks servicesLinks={servicesLinks} />
+                </div>
+
+                {/* Контакты */}
+                <div className="space-y-3">
+                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                        Контакты
+                    </h3>
+                    <FooterContacts className="mt-4" />
+                </div>
+            </div>
+
+            {/* Копирайт */}
+            <div
+                className="border-t border-gray-200 dark:border-gray-800 mt-12 pt-8 text-center text-gray-500 dark:text-gray-400 text-sm">
+                © {currentYear} Рекламастер. Все права защищены.
             </div>
         </footer>
     )
