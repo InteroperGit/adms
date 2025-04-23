b092827062832ad8fd0e43bc57028713565e87db
Maxim Matveev <maxx.matveev@gmail.com>
2025-04-24 00:54:12 +0300

    [AdvAgencySite] [Feature] Доработана основная страница Портфолио
    - В случае отсутствия категории услуг будет выводиться страница 404

--- src/apps/web/advertise-agency-site/src/app/portfolio/category/[category]/[page]/page.tsx
+++ src/apps/web/advertise-agency-site/src/app/portfolio/category/[category]/[page]/page.tsx
@@ -28,53 +28,55 @@ export async function generateStaticParams(): Promise<PageCategoryParams[]> {
 }
 
 export default async function PortfolioPage(props: PageCategoryProps) {
-        try {
-            const { category } = await props.params;
+    let category: string | null = null;
+    let serviceCategory: ServiceCategory | null = null;
+    let categories: ServiceCategory[] | null = null;
 
-            const serviceCategory: ServiceCategory | null = await getServiceCategoryByName(category);
-            const categories: ServiceCategory[] | null = await getServiceCategories("nonheader");
+    try {
+        category = (await props.params).category;
+        serviceCategory = await getServiceCategoryByName(category);
+        categories = await getServiceCategories("nonheader");
+    } catch (error) {
+        console.error("Ошибка при получении данных с сервера:", error);
+        return (
+            <div className="min-h-screen flex items-center justify-center text-center p-4">
+                <h1 className="text-xl text-red-600">Сервис временно недоступен. Попробуйте позже.</h1>
+            </div>
+        );
+    }
 
-            const serviceCategories: ServiceCategory[] = [
-                { id: 1, name: SERVICE_CATEGORY_ALL_NAME, title: 'Все работы', href: `/portfolio/category/${SERVICE_CATEGORY_ALL_NAME}/1`},
-                ...(categories ?? []).map(item => ({
-                    id: item.id,
-                    name: item.name,
-                    title: item.title,
-                    description: item.description,
-                    href: item.href ? `/portfolio/category/${item.name}/1` : undefined,
-                })),
-            ]
+    if (category !== SERVICE_CATEGORY_ALL_NAME && !serviceCategory) {
+        notFound();
+    }
 
-            if (category !== SERVICE_CATEGORY_ALL_NAME && !serviceCategory) {
-                notFound();
-            }
+    const serviceCategories: ServiceCategory[] = [
+        { id: 1, name: SERVICE_CATEGORY_ALL_NAME, title: 'Все работы', href: `/portfolio/category/${SERVICE_CATEGORY_ALL_NAME}/1`},
+        ...(categories ?? []).map(item => ({
+            id: item.id,
+            name: item.name,
+            title: item.title,
+            description: item.description,
+            href: item.href ? `/portfolio/category/${item.name}/1` : undefined,
+        })),
+    ]
 
-            return (
-                <div className={cn("bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen")}>
-                    {/* Hero секция */}
-                    <HeroSection
-                        title={"Наши работы"}
-                        description={category === SERVICE_CATEGORY_ALL_NAME
-                            ? 'Реализованные проекты за последние годы'
-                            : `Проекты в категории "${serviceCategory?.title}"`}
-                    />
+    return (
+        <div className={cn("bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen")}>
+            {/* Hero секция */}
+            <HeroSection
+                title={"Наши работы"}
+                description={category === SERVICE_CATEGORY_ALL_NAME
+                    ? 'Реализованные проекты за последние годы'
+                    : `Проекты в категории "${serviceCategory?.title}"`}
+            />
 
-                    {/* Фильтры */}
-                    <section className={cn("py-12 px-2 md:px-6")}>
-                        <ServiceCategoryFilter
-                            categories={serviceCategories}
-                            activeCategory={category}
-                        />
-                    </section>
-                </div>
-            );
-        } catch (error) {
-            console.error("Ошибка при получении данных с сервера:", error);
-            // Показываем ошибку при отказе от подключения или другим причинам
-            return (
-                <div className="min-h-screen flex items-center justify-center text-center p-4">
-                    <h1 className="text-xl text-red-600">Сервис временно недоступен. Попробуйте позже.</h1>
-                </div>
-            );
-        }
+            {/* Фильтры */}
+            <section className={cn("py-12 px-2 md:px-6")}>
+                <ServiceCategoryFilter
+                    categories={serviceCategories}
+                    activeCategory={category}
+                />
+            </section>
+        </div>
+    );
 }
