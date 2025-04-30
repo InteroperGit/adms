6651ba6b52a0053fafa6138551847bc28262a687
Maxim Matveev <maxx.matveev@gmail.com>
2025-05-01 01:56:04 +0300

    [AdvAgencySite] [Feature] Добавлен функционал получения данных о преимуществах компании из Strapi
    - Добавлен companyAdvantagesApi.ts
    - Добавлены типы companyAdvantageItem.ts, strapiCompanyAdvantage.ts
    - Доработан компонент AdvantagesSection.tsx
    - Доработана главная страница

---
+++ src/apps/web/advertise-agency-site/src/libs/api/companyAdvantagesApi.ts
@@ -0,0 +1,36 @@
+import {CompanyAdvantageItem} from "@/types/companyAdvantageItem";
+import {StrapiCompanyAdvantage} from "@/types/strapi/strapiCompanyAdvantage";
+
+const STRAPI_URL = process.env.INTERNAL_STRAPI_URL;
+
+/**
+ * Получить список преимуществ компании
+ *
+ * Выполняет запрос к Strapi API для получения всех объектов типа "company-advantages"
+ * с вложенными связанными данными (используется `customPopulate=nested`).
+ *
+ * Используется для отображения преимуществ компании на сайте — например, в разделе "Почему мы?".
+ * Каждый элемент преобразуется в тип `CompanyAdvantageItem` и включает id, slug, title и description.
+ *
+ * Данные кэшируются с повторной валидацией каждые 60 секунд.
+ */
+export async function getCompanyAdvantages(): Promise<CompanyAdvantageItem[]> {
+    const res = await fetch(
+        `${STRAPI_URL}/api/company-advantages?customPopulate=nested`,
+        { next: { revalidate: 60 } }
+    );
+
+    if (!res.ok) {
+        throw new Error(`Ошибка при получении статей: ${res.statusText}`);
+    }
+
+    const json = await res.json();
+    return await Promise.all(
+        json.data?.map((item: StrapiCompanyAdvantage) => ({
+                id: item.id,
+                slug: item.slug,
+                title: item.title,
+                description: item.description,
+            } as CompanyAdvantageItem
+        )));
+}
