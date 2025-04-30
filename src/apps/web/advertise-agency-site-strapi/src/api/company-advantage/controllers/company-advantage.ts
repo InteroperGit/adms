cf7a719a8ececb27301658ccfe4055ae976e5937
Maxim Matveev <maxx.matveev@gmail.com>
2025-05-01 01:54:29 +0300

    [AdvAgencySite] [Feature] Добавлен тип CompanyAdvantage в Strapi

---
+++ src/apps/web/advertise-agency-site-strapi/src/api/company-advantage/controllers/company-advantage.ts
@@ -0,0 +1,7 @@
+/**
+ * company-advantage controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::company-advantage.company-advantage');
---
+++ src/apps/web/advertise-agency-site-strapi/src/api/company-advantage/routes/company-advantage.ts
@@ -0,0 +1,7 @@
+/**
+ * company-advantage router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::company-advantage.company-advantage');
