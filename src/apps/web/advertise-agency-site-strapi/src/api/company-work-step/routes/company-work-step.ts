4fcfb472e71d8867297d69b9ad399fe716b38d3d
Maxim Matveev <maxx.matveev@gmail.com>
2025-05-06 02:06:47 +0300

    [AdvAgencySite] [Feature] Добавлен тип CompanyWorkStep в Strapi

---
+++ src/apps/web/advertise-agency-site-strapi/src/api/company-work-step/routes/company-work-step.ts
@@ -0,0 +1,7 @@
+/**
+ * company-work-step router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::company-work-step.company-work-step');
---
+++ src/apps/web/advertise-agency-site-strapi/src/api/company-work-step/services/company-work-step.ts
@@ -0,0 +1,7 @@
+/**
+ * company-work-step service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::company-work-step.company-work-step');
