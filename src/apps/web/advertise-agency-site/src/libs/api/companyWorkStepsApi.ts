1462aa808f6b0207b1ee64f1b2d8a7972b4b9bc7
Maxim Matveev <maxx.matveev@gmail.com>
2025-05-06 02:08:29 +0300

    [AdvAgencySite] [Feature] Разработан функционал получения данных CompanyWorkStep из Strapi
    - Реализован API companyWorkStepsApi.ts
    - Реализованы типы companyWorkStep.ts, strapiCompanyWorkStep.ts
    - Доработан компонент WorkStepsSection.tsx
    - Доработана главная страница:
     - получение данных
     - передача в компонент
    - Удален тестовый источник данных
    - Рефакторинг

---
+++ src/apps/web/advertise-agency-site/src/libs/api/companyWorkStepsApi.ts
@@ -0,0 +1,31 @@
+import {CompanyWorkStep} from "@/types/companyWorkStep";
+import {StrapiCompanyWorkStep} from "@/types/strapi/strapiCompanyWorkStep";
+
+const STRAPI_URL = process.env.INTERNAL_STRAPI_URL;
+
+/**
+ * Получить список шагов выполнения работы компании.
+ * Этот метод выполняет запрос к API Strapi для получения всех шагов выполнения работы компании.
+ * Запрос включает в себя параметр сортировки по полю `stepNumber`, чтобы результаты были отсортированы по возрастанию этого поля.
+ * Метод обрабатывает ответ от API, конвертируя его в формат, соответствующий типу `CompanyWorkStep`,
+ * и возвращает массив шагов, содержащий поля: `title`, `description` и `stepNumber`.
+ */
+export async function getCompanyWorkSteps(): Promise<CompanyWorkStep[]> {
+    const res = await fetch(
+        `${STRAPI_URL}/api/company-work-steps?customPopulate=nested&sort=stepNumber`,
+        { next: { revalidate: 60 } }
+    );
+
+    if (!res.ok) {
+        throw new Error(`Ошибка при получении рабочих шагов компании: ${res.statusText}`);
+    }
+
+    const json = await res.json();
+    return Promise.all(
+        json.data?.map((item: StrapiCompanyWorkStep) => ({
+                title: item.title,
+                description: item.description,
+                stepNumber: item.stepNumber,
+            } as CompanyWorkStep
+        )));
+}
