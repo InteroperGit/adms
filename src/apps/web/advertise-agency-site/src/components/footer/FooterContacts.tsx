db89010caa8a986e0043d02483fe5ae7840a3641
Maxim Matveev <maxx.matveev@gmail.com>
2025-05-09 01:29:41 +0300

    [AdvAgencySite] [Feature] Разработан функционал получения данных ContactItem из Strapi
    - Реализован API contactsApi.ts
    - Реализованы типы contacts.ts, strapiContacts.ts
    - Доработан компонент FooterContacts.tsx, Footer.tsx
    - Доработана layout
      Рефакторинг

--- src/apps/web/advertise-agency-site/src/components/footer/FooterContacts.tsx
+++ src/apps/web/advertise-agency-site/src/components/footer/FooterContacts.tsx
@@ -1,74 +1,115 @@
-import {PhoneIcon, EnvelopeIcon, MapPinIcon} from "@heroicons/react/24/outline";
-import {cn} from "@/libs/utils";
-import React from "react";
+import { PhoneIcon, EnvelopeIcon, MapPinIcon } from "@heroicons/react/24/outline";
+import { cn } from "@/libs/utils";
+import React, {JSX} from "react";
+import Link from "next/link";
+import {ContactItem} from "@/types/contacts";
 
-interface ContactItem {
+interface ContactElement {
     icon: React.ReactNode;
-    text: string;
     hoverColor?: string;
-    href?: string;
+    data: ContactItem;
 }
 
 interface ContactsProps {
     className?: string;
     iconClassName?: string;
     textClassName?: string;
-    contacts?: ContactItem[];
+    contacts: ContactItem[];
 }
 
-const contacts_data = [
-    {
-        icon: <PhoneIcon/>,
-        text: "+7 911 505 35 03",
+const contactsData: Record<string, { icon: React.ReactNode, hoverColor: string  }> = {
+    "phone1": {
+        icon: <PhoneIcon />,
         hoverColor: "hover:text-green-600",
-        href: "tel:+79115053503"
     },
-    {
-        icon: <EnvelopeIcon/>,
-        text: "info@rmaster35.ru",
+    "phone2": {
+        icon: <PhoneIcon />,
+        hoverColor: "hover:text-green-600",
+    },
+    "email": {
+        icon: <EnvelopeIcon />,
         hoverColor: "hover:text-blue-600",
-        href: "mailto:info@rmaster35.ru"
     },
-    {
-        icon: <MapPinIcon/>,
-        text: "г. Череповец, ул. Металлургов, д. 9",
+    "address": {
+        icon: <MapPinIcon />,
         hoverColor: "hover:text-red-600",
-        href: "https://yandex.ru/maps/-/CDbQYNRg"
-    }
-];
+    },
+}
 
-export default function FooterContacts({
+const getContactElements = (contacts?: ContactItem[]): ContactElement[] => {
+    if (!contacts) {
+        return [];
+    }
+
+    return contacts.map((contact) => ({
+        icon: contactsData[contact.slug]?.icon,
+        hoverColor: contactsData[contact.slug]?.hoverColor,
+        data: contact
+    }))
+}
+
+/**
+ * Компонент для отображения списка контактных элементов в футере.
+ * Отображает контакты, такие как телефон, email и адрес, с соответствующими иконками и анимацией при наведении.
+ * Если для контакта предоставлен URL, то при клике происходит переход по указанной ссылке.
+ * Контакты могут быть отображены в виде ссылок или просто текста в зависимости от наличия URL.
+ *
+ * @param {ContactsProps} props - Свойства компонента.
+ * @param {string} [props.className] - Дополнительные классы для кастомизации стилей компонента.
+ * @param {string} [props.iconClassName] - Дополнительные классы для иконок.
+ * @param {string} [props.textClassName] - Дополнительные классы для текста.
+ * @param {ContactItem[]} props.contacts - Массив объектов с данными для каждого контакта (например, телефон, email, адрес).
+ *
+ * @returns {JSX.Element} - Разметка для списка контактов, с анимацией и ссылками на социальные сети или контакты.
+ *
+ * Пример использования:
+ * <FooterContacts contacts={contactsData} />
+ */
+const FooterContacts = ({
                                            className,
-                                           contacts = contacts_data
-                                       }: ContactsProps) {
+                                           contacts,
+                                       }: ContactsProps): JSX.Element => {
+    const contactElements = getContactElements(contacts);
+
     return (
-        <>
-            <ul className={cn("space-y-2", className)}>
-                {contacts.map((contact, index) => (
-                    <li key={index}>
-                        <a
-                            href={contact.href}
-                            target={contact.href?.startsWith('http') ? "_blank" : "_self"}
-                            rel="noopener noreferrer"
-                            className="group flex items-start space-x-2"
+        <ul className={cn("space-y-2", className)}>
+            {contactElements.map((contact, index) => (
+                <li key={index}>
+                    <Link
+                        href={contact.data?.url || ""}
+                        target={contact.data?.url?.startsWith("http") ? "_blank" : "_self"}
+                        rel="noopener noreferrer"
+                        className={cn(
+                            "group flex items-start space-x-2",
+                            "relative text-gray-700 dark:text-gray-300",
+                            "hover:text-primary dark:hover:text-primary-400",
+                            "transition-colors duration-200"
+                        )}
+                    >
+                        <span
+                            className={cn("h-5 w-5 text-gray-500 mt-0.5", contact.hoverColor)}
                         >
-                            <span className={cn(
-                                "h-5 w-5 text-gray-500 mt-0.5",
-                                contact.hoverColor
-                            )}>
-                              {contact.icon}
-                            </span>
-                            <span className={cn(
-                                    "text-gray-600 dark:text-gray-400",
-                                    "group-hover:underline",
-                                    "group-hover:underline-offset-4",
-                                )}>
-                              {contact.text}
-                            </span>
-                        </a>
-                    </li>
-                ))}
-            </ul>
-        </>
+                          {contact.icon}
+                        </span>
+                        <span
+                            className={cn(
+                                "inline-block", // Сделаем span строчным элементом
+                                "relative"
+                            )}
+                        >
+                          {contact.data?.info}
+                        <span
+                            className={cn(
+                                "absolute left-0 bottom-[-2px] h-[1px] w-0 bg-orange-500 dark:bg-orange-400",
+                                "transition-all duration-300 group-hover:w-full"
+                            )}
+                        />
+                        </span>
+                    </Link>
+                </li>
+            ))}
+        </ul>
     );
 }
+
+export default FooterContacts;
