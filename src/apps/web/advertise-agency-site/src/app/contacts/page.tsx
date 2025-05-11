4400603b6c18f415ac43b9a1764621cde7d2a702
Maxim Matveev <maxx.matveev@gmail.com>
2025-05-11 23:52:16 +0300

    [AdvAgencySite] [Feature] Доработана страница Контакты
    - Реалмзовано получение контактов из Strapi
    - Доработан компонент ContactsCard.tsx
    - Рефакторинг

--- src/apps/web/advertise-agency-site/src/app/contacts/page.tsx
+++ src/apps/web/advertise-agency-site/src/app/contacts/page.tsx
@@ -2,8 +2,28 @@ import YandexMap from "@/components/misc/YandexMap";
 import ContactsCard from "@/components/cards/ContactsCard";
 import FeedbackForm from "@/components/forms/FeedbackForm";
 import PageHeader from "@/components/header/PageHeader";
+import {JSX} from "react";
+import {ContactItem} from "@/types/contacts";
+import ServiceIsNotRespondedError from "@/components/error/ServiceIsNotRespondedError";
+import {getContacts} from "@/libs/api/contactsApi";
 
-export default function ContactsPage() {
+/**
+ * Метод, который представляет страницу с контактной информацией и формой обратной связи
+ * @constructor
+ */
+const ContactsPage = async (): Promise<JSX.Element> => {
+    let contacts: ContactItem[];
+
+    try {
+        contacts = await getContacts();
+    }
+    catch (error) {
+        console.error("Ошибка при получении данных с сервера:", error);
+        return (
+            <ServiceIsNotRespondedError />
+        )
+    }
+
     return (
         <>
             <PageHeader>Контакты</PageHeader>
@@ -11,7 +31,7 @@ export default function ContactsPage() {
             <div className={"grid grid-cols-1 md:grid-cols-2 gap-8"}>
                 {/* Левая колонка - контактная информация */}
                 <div className={"space-y-6"}>
-                    <ContactsCard />
+                    <ContactsCard contacts={contacts} />
 
                     <YandexMap
                         constructorId="8e9d4dd92e269a69df84774136ae2871466aa55b34f941cb126cfc03efab1fb8"
@@ -25,3 +45,5 @@ export default function ContactsPage() {
         </>
     );
 }
+
+export default ContactsPage;
--- src/apps/web/advertise-agency-site/src/components/cards/ContactsCard.tsx
+++ src/apps/web/advertise-agency-site/src/components/cards/ContactsCard.tsx
@@ -4,8 +4,63 @@ import {
     CardContent,
     CardTitle,
 } from "@/components/ui/card"
+import React, {JSX} from "react";
+import {ContactItem} from "@/types/contacts";
+import {cn} from "@/libs/utils";
+import Link from "next/link";
 
-export default function ContactsCard() {
+interface ContactElement {
+    icon: React.ReactNode;
+    hoverColor: string;
+    data: ContactItem;
+}
+
+const contactsData: Record<string, { icon: React.ReactNode, hoverColor: string  }> = {
+    "phone1": {
+        icon: <PhoneIcon />,
+        hoverColor: "hover:text-green-600",
+    },
+    "phone2": {
+        icon: <PhoneIcon />,
+        hoverColor: "hover:text-green-600",
+    },
+    "email": {
+        icon: <EnvelopeIcon />,
+        hoverColor: "hover:text-blue-600",
+    },
+    "address": {
+        icon: <MapPinIcon />,
+        hoverColor: "hover:text-red-600",
+    },
+}
+
+const getContactElements = (contacts?: ContactItem[]): ContactElement[] => {
+    if (!contacts) {
+        return [];
+    }
+
+    return contacts.map((contact) => ({
+        icon: contactsData[contact.slug]?.icon,
+        hoverColor: contactsData[contact.slug]?.hoverColor,
+        data: contact
+    }))
+}
+
+interface ContactCardProps {
+    contacts: ContactItem[];
+}
+
+/**
+ * Компонент карточки с контактной информацией, который отображает адрес, телефон и email.
+ *
+ * @returns {JSX.Element} - Разметка карточки с контактной информацией.
+ *
+ * Пример использования:
+ * <ContactsCard />
+ */
+const ContactsCard = ({ contacts }: ContactCardProps): JSX.Element => {
+    const contactElements = getContactElements(contacts);
+
     return (
         <Card>
             <CardTitle>
@@ -14,43 +69,49 @@ export default function ContactsCard() {
 
             <CardContent>
                 <ul className={"space-y-4"}>
-                    <li className={"flex items-start gap-4"}>
-                        <MapPinIcon className="h-6 w-6 text-gray-500 mt-0.5 flex-shrink-0" />
-                        <div>
-                            <h3 className={"font-medium"}>Адрес</h3>
-                            <p className={"text-gray-600 dark:text-gray-400"}>
-                                г. Череповец, ул. Металлургов, д. 9
-                            </p>
-                        </div>
-                    </li>
+                    {
+                        contactElements.map((contact) => (
+                            <li key={contact.data.id} className={"flex items-center gap-6"}>
+                                <Link
+                                    href={contact.data?.url || ""}
+                                    target={contact.data?.url?.startsWith("http") ? "_blank" : "_self"}
+                                    rel="noopener noreferrer"
+                                    className={cn(
+                                        "group flex items-end space-x-4", // Выравнивание по центру и увеличение расстояния между элементами
+                                        "relative text-gray-700 dark:text-gray-300",
+                                        "hover:text-primary dark:hover:text-primary-400",
+                                        "transition-colors duration-200"
+                                    )}
+                                >
+                                    <span
+                                        className={cn(
+                                            "flex-shrink-0", // Иконка не будет сжиматься
+                                            "h-6 w-6",
+                                            "mt-0.5 text-gray-500", // Убедитесь, что иконка будет в нужном размере
+                                            contact.hoverColor, // Цвет при наведении, передается как класс
+                                            "transition-colors duration-200" // Плавное изменение цвета
+                                        )}
+                                    >
+                                      {contact.icon}
+                                    </span>
 
-                    <li className="flex items-start gap-4">
-                        <PhoneIcon className="h-6 w-6 text-gray-500 mt-0.5 flex-shrink-0" />
-                        <div>
-                            <h3 className="font-medium">Телефон</h3>
-                            <a
-                                href="tel:+79115053503"
-                                className="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white transition-colors"
-                            >
-                                +7 (911) 505-35-03
-                            </a>
-                        </div>
-                    </li>
-
-                    <li className="flex items-start gap-4">
-                        <EnvelopeIcon className="h-6 w-6 text-gray-500 mt-0.5 flex-shrink-0" />
-                        <div>
-                            <h3 className="font-medium">Email</h3>
-                            <a
-                                href="mailto:info@rmaster35.ru"
-                                className="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white transition-colors"
-                            >
-                                info@rmaster35.ru
-                            </a>
-                        </div>
-                    </li>
+                                    <span className="relative">
+                                        {contact.data?.info}
+                                        <span
+                                            className={cn(
+                                                "absolute left-0 bottom-[-2px] h-[1px] w-0 bg-orange-500 dark:bg-orange-400",
+                                                "transition-all duration-300 group-hover:w-full"
+                                            )}
+                                        />
+                                    </span>
+                                </Link>
+                            </li>
+                        ))
+                    }
                 </ul>
             </CardContent>
         </Card>
     );
 }
+
+export default ContactsCard;
