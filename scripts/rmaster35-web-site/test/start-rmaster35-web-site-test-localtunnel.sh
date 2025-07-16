eb08cf859e6029d0f0772aa3c09be7f134a2a28d
Maxim Matveev <maxx.matveev@gmail.com>
2025-07-16 19:52:17 +0300

    [AdvAgencySite] [Feature] Доработаны скрипты для запуска/останова инфраструктуры сайта

--- scripts/rmaster35-web-site/test/start-rmaster35-web-site-test-localtunnel.sh
+++ scripts/rmaster35-web-site/test/start-rmaster35-web-site-test-localtunnel.sh
@@ -27,11 +27,54 @@ docker-compose -f "$COMPOSE_FILE" up --build -d
 
 echo "✅ Контейнеры $SERVICE_NAME перезапущены."
 
+echo "📦 Ожидание запуска Strapi..."
+
 # Ждем доступность Strapi
 until curl -s http://localhost:1337; do
   echo "Жду Strapi..."
   sleep 2
 done
+
+echo "✅ Strapi запущен."
+
+# -----------------------------------------------------------------
+# Запускаем backend
+# -----------------------------------------------------------------
+
+cd ../..
+
+# Путь к каталогу с docker-compose файлом
+COMPOSE_DIR="prod/backend"
+COMPOSE_FILE="docker-compose.yml"
+
+# Имя сервиса
+SERVICE_NAME="rmaster35-web-site-backend"
+
+echo "📦 Сборка $SERVICE_NAME через docker-compose..."
+
+# Переходим в каталог с docker-compose
+cd "$COMPOSE_DIR" || {
+  echo "❌ Не удалось перейти в каталог $COMPOSE_DIR"
+  exit 1
+}
+
+echo "📦 Останавливаем, удаляем предыдущие контейнеры..."
+
+# Останавливаем и удаляем контейнеры, затем пересобираем и запускаем
+docker-compose -f "$COMPOSE_FILE" down --remove-orphans
+docker container prune -f
+docker image prune -f
+docker-compose -f "$COMPOSE_FILE" up --build -d
+
+echo "✅ Контейнеры $SERVICE_NAME перезапущены."
+
+# Ждем доступность служб
+until [ "$(curl -s -o /dev/null -w '%{http_code}' http://localhost:3002/health)" -eq 200 ]; do
+  echo "Жду ApiGatewayService..."
+  sleep 2
+done
+
+echo "✅ Службы backend запущены."
 
 # -----------------------------------------------------------------
 # Запускаем NextJS build
@@ -86,7 +129,7 @@ COMPOSE_DIR="test/localtunnel"
 # Имя сервиса
 SERVICE_NAME="rmaster35-web-site-test-localtunnel"
 
-echo "📦 Перезапуск контейнеров $SERVICE_NAME через docker-compose..."
+echo "📦 Запуск сайта rmaster35-web-site..."
 
 # Переходим в каталог с docker-compose
 cd "$COMPOSE_DIR" || {
@@ -107,4 +150,10 @@ cp -r ../../site-build/next-build/output .
 
 docker-compose -f "$COMPOSE_FILE" up --build -d
 
-echo "✅ Контейнер $SERVICE_NAME перезапущен."
+# Ждем, пока порт поднимется
+until [ "$(curl -s -o /dev/null -w '%{http_code}' http://localhost:3000)" = "200" ]; do
+  echo "Жду запуска сайта..."
+  sleep 5
+done
+
+echo "✅ Сайт rmaster35-web-site запущен."
--- scripts/rmaster35-web-site/test/stop-rmaster35-web-site-test-localtunnel.sh
+++ scripts/rmaster35-web-site/test/stop-rmaster35-web-site-test-localtunnel.sh
@@ -4,6 +4,10 @@
 COMPOSE_DIR="../../../deploy/docker/rmaster35-web-site/test/strapi"
 COMPOSE_FILE="docker-compose.yml"
 
+# -----------------------------------------------------------------
+# Останавливаем Strapi
+# -----------------------------------------------------------------
+
 # Имя сервиса
 SERVICE_NAME="rmaster35-web-site-test-strapi"
 
@@ -16,9 +20,15 @@ cd "$COMPOSE_DIR" || {
 }
 
 # Останавливаем и удаляем контейнеры, затем пересобираем и запускаем
-docker-compose -f "$COMPOSE_FILE" down
+docker-compose -f "$COMPOSE_FILE" down --remove-orphans
+docker container prune -f
+docker image prune -f
 
 echo "✅ Контейнеры $SERVICE_NAME остановлены."
+
+# -----------------------------------------------------------------
+# Останавливаем rmaster35-web-site
+# -----------------------------------------------------------------
 
 cd ..
 
@@ -37,6 +47,35 @@ cd "$COMPOSE_DIR" || {
 }
 
 # Останавливаем и удаляем контейнеры, затем пересобираем и запускаем
-docker-compose -f "$COMPOSE_FILE" down
+docker-compose -f "$COMPOSE_FILE" down --remove-orphans
+docker container prune -f
+docker image prune -f
+
+echo "✅ Контейнеры $SERVICE_NAME остановлены."
+
+# -----------------------------------------------------------------
+# Останавливаем rmaster35-web-site backend
+# -----------------------------------------------------------------
+
+cd ../..
+
+# Путь к каталогу с docker-compose файлом
+COMPOSE_DIR="prod/backend"
+
+# Имя сервиса
+SERVICE_NAME="rmaster35-web-site-backend"
+
+echo "📦 Останов контейнеров $SERVICE_NAME через docker-compose..."
+
+# Переходим в каталог с docker-compose
+cd "$COMPOSE_DIR" || {
+  echo "❌ Не удалось перейти в каталог $COMPOSE_DIR"
+  exit 1
+}
+
+# Останавливаем и удаляем контейнеры, затем пересобираем и запускаем
+docker-compose -f "$COMPOSE_FILE" down --remove-orphans
+docker container prune -f
+docker image prune -f
 
 echo "✅ Контейнеры $SERVICE_NAME остановлены."
